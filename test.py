a83c403b8a5723829462b397215a396a76d31d51
abhi6059 <abhi6059>
2023-08-02 05:56:57 +0000

    Updated addition and added sub

--- test.py
+++ test.py
@@ -10,11 +10,12 @@ def test_func():
 # COMMAND ----------
 
 def test_func_v2():
-    print(2+4)
+    print(2+3)
     print(3-1)
     print(2*2)
     print("Adding another function from v2 to test sync of branch behavior")
     print(5+3)
+    print(5-2)
 
 # COMMAND ----------
 
