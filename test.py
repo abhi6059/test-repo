8a9bbddfca4bf923a2ead3ccff5fd45d34845b75
abhi6059 <abhi6059>
2023-08-02 06:07:45 +0000

    Modified addition and added division
    Modified addition and added division

--- test.py
+++ test.py
@@ -10,9 +10,10 @@ def test_func():
 # COMMAND ----------
 
 def test_func_v2():
-    print(2+3)
+    print(2+5)
     print(3-1)
     print(2*2)
+    print(4/2)
     print("Adding another function from v2 to test sync of branch behavior")
     print(5+3)
     print(5-2)
