3e9d1a956c0dd62d3d41a901e1a4fe8cfb7280e1
abhi6059 <abhi6059>
2023-08-02 04:54:35 +0000

    Added another function from v2 to test sync

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ def main():
 
 def test_func():
     print("This is function to test branch behavior")
+
+# COMMAND ----------
+
+def test_func_v2():
+    print("Adding another function from v2 to test sync of branch behavior")
